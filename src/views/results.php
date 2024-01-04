9f914e8037eb7f88274c7a895ac618c1125fe19d
francus11 <78595332+francus11@users.noreply.github.com>
2024-01-04 17:04:26 +0100

    created search results page

---
+++ src/views/results.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/results.css">
+    <link rel="stylesheet" type="text/css" href="/public/fontello/css/fontello.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <title>Results</title>
+</head>
+<body>
+    <header></header>
+    <div id="content">
+        <div id="results-header">
+            <div id="result-counter">Results: 34</div>
+            <div class="common-button">Edit</div>
+        </div>
+        <div class="result-item">
+            <div class="result-item-photo"></div>
+            <div class="result-item-texts">
+                <div class="result-item-title">Refrigerator RefrigeratorRefrigerator RefrigeratorRefrigerator Refrigerator </div>
+                <div class="result-item-price">123.45 zł</div>
+            </div>
+            <div class="result-item-buttons">
+                <i class="icon-heart-empty"></i>
+                <i class="icon-eye-off"></i>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+</body>
+</html>
