290300637ac02a44da8e30c6e711ef9eaf58ab91
adrian.cholody <adriancholody@gmail.com>
2024-01-09 23:57:33 +0100

    created view for list of patterns

---
+++ src/views/patterns.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/patterns.css">
+    <link rel="stylesheet" type="text/css" href="/public/fontello/css/fontello.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <title>Your patterns</title>
+</head>
+<body>
+    <header></header>
+    <div id="content">
+        <div id="patterns-header">
+            <div id="title">Patterns</div>
+            <div class="common-button">Create new patters</div>
+        </div>
+    </div>
+    <footer></footer>
+</body>
+</html>
