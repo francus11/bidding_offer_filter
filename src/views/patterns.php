449b7930a197f4a9a601ee82368290e97c5ddce9
adrian.cholody <adriancholody@gmail.com>
2024-01-14 19:33:47 +0100

    Created patterns view

--- src/views/patterns.php
+++ src/views/patterns.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/public/css/style.css">
     <link rel="stylesheet" type="text/css" href="/public/css/patterns.css">
     <link rel="stylesheet" type="text/css" href="/public/fontello/css/fontello.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
     <title>Your patterns</title>
 </head>
+
 <body>
     <header></header>
     <div id="content">
@@ -18,7 +20,21 @@
             <div id="title">Patterns</div>
             <div class="common-button">Create new patters</div>
         </div>
+        <div id="patterns">
+            <div class="pattern">
+                <div class="upper">
+                    <div class="pattern-title">Refrigerator</div>
+                    <div class="common-button">Edit</div>
+                </div>
+                <div class="lower">
+                    <div class="offers-count new">New: 3</div>
+                    <div class="offers-count">All: 22</div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
     <footer></footer>
 </body>
+
 </html>
