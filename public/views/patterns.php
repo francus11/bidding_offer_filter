e73d385badf483b2fcacfe750b8c5f61db6507bc
adrian.cholody <adriancholody@gmail.com>
2024-01-18 20:57:15 +0100

    moved patterns to public/views

--- public/views/patterns.php
+++ public/views/patterns.php
@@ -2,19 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+    <?php
+    include 'common/common_head.php';
+    ?>
     <link rel="stylesheet" type="text/css" href="/public/css/patterns.css">
-    <link rel="stylesheet" type="text/css" href="/public/fontello/css/fontello.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
     <title>Your patterns</title>
 </head>
 
 <body>
-    <header></header>
+    <?php
+    include 'common/header.php';
+    ?>
     <div id="content">
         <div id="patterns-header">
             <div id="title">Patterns</div>
@@ -33,8 +31,10 @@
             </div>
         </div>
     </div>
-    </div>
-    <footer></footer>
+
+    <?php
+    include 'common/footer.php';
+    ?>
 </body>
 
 </html>
