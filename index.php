0a4c4ec66d4889f687a0d2e1437f2aa9119289a0
adrian.cholody <adriancholody@gmail.com>
2024-01-20 14:08:52 +0100

    Added new routing

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $path = parse_url($path, PHP_URL_PATH);
 Router::get('', 'DefaultController');
 Router::post('login', 'SecurityController');
 Router::post('register', 'SecurityController');
+Router::post('new_pattern', 'PatternController');
 
 Router::run($path);
