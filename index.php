74b57c1d7ee118013f135f2c9d616bd007ca0504
adrian.cholody <adriancholody@gmail.com>
2024-01-22 22:43:11 +0100

    Added routing to offer listing

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
+Router::get('offers', 'PatternController');
 Router::post('login', 'SecurityController');
 Router::post('register', 'SecurityController');
 Router::post('new_pattern', 'PatternController');
