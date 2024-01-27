b434abdbf44d9aec095d11e27b922acdd3a9736e
adrian.cholody <adriancholody@gmail.com>
2024-01-27 11:40:11 +0100

    Created primitive main site and brand identification

--- public/views/common/header.php
+++ public/views/common/header.php
@@ -1,7 +1,13 @@
 <header>
-    <div id="links">
-        <a href="favourite">Favourite</a>
-        <a href="patterns">Patterns</a>
-        <a href="login">Login/register</a>
+    <div class="header-box">
+        <div class="header-square">
+            <img src="/public/views/common/logo_white.png" alt="text">
+        </div>
+        <div id="links">
+            <a href="favourite">Favourite</a>
+            <a href="patterns">Patterns</a>
+            <a href="login">Account</a>
+        </div>
+        <div class="header-square"></div>
     </div>
-</header>
+</header>   
---
+++ public/views/main.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include 'common/common_head.php';
+    ?>
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
+    <title>Results</title>
+</head>
+
+<body>
+    <?php
+    include 'common/header.php';
+    ?>
+    <div id="content">
+        <div class="tile">
+            <div class="text-box">
+                <h1>Buy items cheaper and easier with Bidder</h1>
+                <p>Now you don't have to search through multiple of products pages to find your ideal offer with ideal price. Bidder will help you filter through offers from many popular platforms.</p>
+            </div>
+            <div class="logo-img">
+                <img src="/public/views/common/logo_white.png" alt="">
+            </div>
+        </div>
+        <div class="tile">
+            <div class="button-box">
+                <div class="button-redirect">
+                    Try now!
+                </div>
+            </div>
+            <div class="text-box right-justify bigger">
+                <p>
+                    Create patterns to search easily <br> Apply custom filters to pick best offers <br> Merge patterns to keep similar queries together
+                </p>
+            </div>
+        </div>
+    </div>
+    <?php
+    include 'common/footer.php';
+    ?>
+</body>
+
+</html>
