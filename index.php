1249253cd83d88ebf5071f0eceecfb80099c8594
adrian.cholody <adriancholody@gmail.com>
2024-01-28 14:59:15 +0100

    modified routing

--- index.php
+++ index.php
@@ -7,8 +7,14 @@ $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
 Router::get('offers', 'PatternController');
+Router::get('favourite', 'PatternController');
+Router::get('patterns', 'PatternController');
 Router::get('login', 'SecurityController');
 Router::get('logout', 'SecurityController');
+Router::get('account', 'SecurityController');
+Router::get('cat', 'APIController');
+
+
 Router::post('login', 'SecurityController');
 Router::post('register', 'SecurityController');
 Router::post('new_pattern', 'PatternController');
